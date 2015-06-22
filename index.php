e437e6c44050e144e112e7e43239a47c2f6e443a
Ivan Jaramillo <converse_ivan@hotmail.com>
2015-06-21 20:02:44 -0500

    Index de la aplicacion de webservices basico

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ $curl = curl_init("http://localhost/services/basico/base.txt");
+
+ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+ $respuesta = curl_exec($curl);
+ $info = curl_getinfo($curl);
+
+
+ if($info['http_code'] == 200){
+ 	$datos = explode(",", $respuesta);
+
+ 	echo "<h1>Furtas en mi tienda</h1>";
+
+ 	foreach ($datos as $fruta) {
+ 		echo "-> ".$fruta."<br>";
+ 	}
+ }else{
+ 	echo "Error".curl_error($curl);
+ }
+
+
+?>
